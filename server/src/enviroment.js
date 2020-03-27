96ac70a16891f8976a021fffcacdfaacb1cc147d
Belchior Oliveira <belchior@outlook.com>
2020-03-27 14:58:48 -0300

    [server] improved server setup

--- server/src/enviroment.js
+++ server/src/enviroment.js
@@ -3,4 +3,5 @@ export const NODE_ENV = process.env.NODE_ENV || 'development';
 export const DB_CONNECTION = process.env.DB_CONNECTION || 'mongodb://localhost:27017/company';
 export const DEBUG = process.env.DEBUG;
 export const PORT = process.env.PORT || 4000;
-export const ORIGIN = process.env.ORIGIN || `http://localhost:${PORT}/graphql`;
+export const ORIGIN = process.env.ORIGIN || `http://localhost:${PORT}`;
+export const FRONTEND_ORIGIN = process.env.FRONTEND_ORIGIN || 'http://localhost:3000';
--- server/src/server.js
+++ server/src/server.js
@@ -1,20 +1,22 @@
+import cors from 'cors';
 import express from 'express';
 import graphqlHTTP from 'express-graphql';
 import mongoose from 'mongoose';
 
-import { DB_CONNECTION, DEBUG, NODE_ENV, ORIGIN, PORT } from './enviroment';
+import { DB_CONNECTION, DEBUG, FRONTEND_ORIGIN, NODE_ENV, ORIGIN, PORT } from './enviroment';
 import { schema } from './schema';
 
 
 const app = express();
 
-const server = () => {
+const startServer = () => {
+  app.use(cors({ origin: FRONTEND_ORIGIN }));
   app.use('/graphql', graphqlHTTP({
     schema: schema,
     graphiql: NODE_ENV === 'development',
   }));
   app.listen(PORT, () => {
-    console.log(`Running a GraphQL API server at ${ORIGIN}`);
+    console.log(`Running a GraphQL API server at ${ORIGIN}/graphql`);
   });
 };
 
@@ -27,5 +29,5 @@ const connectionOptions = {
 
 mongoose.set('debug', DEBUG === 'db' || DEBUG === '*');
 mongoose.connect(DB_CONNECTION, connectionOptions);
-mongoose.connection.once('open', server);
+mongoose.connection.once('open', startServer);
 mongoose.connection.on('error', console.error.bind(console, 'connection error:'));
