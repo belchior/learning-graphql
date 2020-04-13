70853650bb5b36004026dca03deb7c9526dc7753
Belchior Oliveira <belchior@outlook.com>
2020-04-13 18:01:06 -0300

    [client] removed eslint plugin

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -6,7 +6,6 @@ module.exports = {
   },
   'plugins': [
     'jest',
-    'no-null',
   ],
   'extends': [
     'react-app',
@@ -25,7 +24,6 @@ module.exports = {
     'indent': ["error", 2, { SwitchCase: 1 }],
     'linebreak-style': [ 'error', 'unix' ],
     'max-len': ['error', { code: 120, ignoreComments: true }],
-    'no-null/no-null': 2,
     'object-curly-spacing': [ 'error', 'always' ],
     'quotes': [ 'error', 'single' ],
     'semi': [ 'error', 'always' ],
