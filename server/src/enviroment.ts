5c38613b82a2297714b476104d298f2353d78054
Belchior Oliveira <belchior@outlook.com>
2020-05-08 11:17:05 -0300

    [server] converted Dockerfile to run in production

--- server/src/enviroment.ts
+++ server/src/enviroment.ts
@@ -1,7 +1,7 @@
 
-export const NODE_ENV = process.env.NODE_ENV || 'development';
+export const CLIENT_URL = process.env.CLIENT_URL || 'http://localhost:3000';
 export const DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost:27017/learning-graphql';
 export const DEBUG = process.env.DEBUG;
+export const NODE_ENV = process.env.NODE_ENV || 'development';
 export const PORT = process.env.PORT || 4000;
 export const SERVER_URL = process.env.SERVER_URL || `http://localhost:${PORT}`;
-export const CLIENT_URL = process.env.CLIENT_URL || 'http://localhost:3000';
