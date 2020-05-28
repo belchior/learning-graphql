553fafcffaa5d01f2ede254fca46e30294fd0a29
Belchior Oliveira <belchior@outlook.com>
2020-05-28 16:06:19 -0300

    [client] improved deploy configuration

--- client/src/utils/environment.tsx
+++ client/src/utils/environment.tsx
@@ -1,6 +1,6 @@
 import { Environment, Network, RecordSource, Store, } from 'relay-runtime';
 
-const endpoint = process.env.REACT_APP_SERVER_URL;
+const endpoint = process.env.REACT_APP_HOST_SERVER || '';
 
 const fetchQuery = async (operation: { text: string }, variables: object) => {
   return fetch(`${endpoint}/graphql`, {
